bf5f7e8430fe2a3933c5aa15bab3a677beab233a
anhquocpwq <anhquocpwq@gmail.com>
2025-10-19 08:18:14 +0700

    nice code

--- hell.py
+++ hell.py
@@ -1,1 +1,9 @@
-print("hell")
+from google import genai
+
+# The client gets the API key from the environment variable `GEMINI_API_KEY`.
+client = genai.Client(api_key="AIzaSyAtzWm83cMs_JdwTeiZel6QXu12KjqzIQ8")
+while True:
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", contents=input("Enter your prompt: ")
+    )
+    print(response.text)
